eb33eb3690e7a0e73da91348afee1641f5475c5d
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-02-29 18:48:42 -0800

    More work on JSON measurements

--- src/main/java/com/cowtowncoder/microb/jackson/CustomJsonNodeReader.java
+++ src/main/java/com/cowtowncoder/microb/jackson/CustomJsonNodeReader.java
@@ -1,24 +1,114 @@
 package com.cowtowncoder.microb.jackson;
 
 import java.io.IOException;
-import java.io.InputStream;
 
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class CustomJsonNodeReader
+public final class CustomJsonNodeReader
 {
-    private final ObjectMapper _mapper;
+    private final JsonNodeFactory _nodes;
+    private final JsonParser _parser;
 
-    public CustomJsonNodeReader(ObjectMapper m) {
-        _mapper = m;
+    private final boolean _cfgFloatsAsBigDecimal;
+    
+    public CustomJsonNodeReader(ObjectMapper m, JsonParser p) {
+        _nodes = m.getNodeFactory();
+        _parser = p;
+        _cfgFloatsAsBigDecimal = m.isEnabled(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
     }
 
-    public JsonNode readTree(InputStream in) {
+    public JsonNode readTree() {
         try {
-            return _mapper.readTree(in);
+            _parser.nextToken();
+            return _readFromRoot();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private JsonNode _readFromRoot() throws IOException
+    {
+        switch (_parser.currentTokenId()) {
+        case JsonTokenId.ID_START_OBJECT:
+            return _readObject(null);
+        case JsonTokenId.ID_END_OBJECT:
+            // Is this even allowed?
+            return _nodes.objectNode();
+        case JsonTokenId.ID_START_ARRAY:
+            return _readArray(null);
+        }
+        return _readAny(null);
+    }
+
+    private JsonNode _readObject(String parentProp) throws IOException
+    {
+        ObjectNode ob = _nodes.objectNode();
+        String propName;
+
+        while ((propName = _parser.nextFieldName()) != null) {
+            _parser.nextToken();
+            ob.set(propName, _readAny(propName));
+        }
+
+        if (_parser.currentTokenId() != JsonTokenId.ID_END_OBJECT) {
+            throw _readError("Internal state error: current token should be END_OBJECT, was: "
+                    +_parser.currentToken());
+        }
+        return ob;
+    }
+
+    private JsonNode _readArray(String parentProp) throws IOException
+    {
+        ArrayNode arr = _nodes.arrayNode();
+        while (_parser.nextToken() != JsonToken.END_ARRAY) {
+            arr.add(_readAny(null));
+        }
+        return arr;
+    }
+
+    private JsonNode _readAny(String parentProp) throws IOException
+    {
+        switch (_parser.currentTokenId()) {
+        case JsonTokenId.ID_START_OBJECT:
+            return _readObject(parentProp);
+        case JsonTokenId.ID_START_ARRAY:
+            return _readArray(parentProp);
+        case JsonTokenId.ID_STRING:
+            return _nodes.textNode(_parser.getText());
+        case JsonTokenId.ID_NUMBER_INT:
+            JsonParser.NumberType nt = _parser.getNumberType();
+            if (nt == JsonParser.NumberType.INT) {
+                return _nodes.numberNode(_parser.getIntValue());
+            }
+            if (nt == JsonParser.NumberType.LONG) {
+                return _nodes.numberNode(_parser.getLongValue());
+            }
+            return _nodes.numberNode(_parser.getBigIntegerValue());
+        case JsonTokenId.ID_NUMBER_FLOAT:
+            if (_cfgFloatsAsBigDecimal) {
+                return _nodes.numberNode(_parser.getDecimalValue());
+            }
+            return _nodes.numberNode(_parser.getDoubleValue());
+        case JsonTokenId.ID_TRUE:
+            return _nodes.booleanNode(true);
+        case JsonTokenId.ID_FALSE:
+            return _nodes.booleanNode(false);
+        case JsonTokenId.ID_NULL:
+            return _nodes.nullNode();
+        default:
+        }
+        throw _readError("Internal state error: current token type unsupported: "
+                +_parser.currentToken());
+    }
+
+    private StreamReadException _readError(String msg) {
+        return new JsonParseException(_parser, msg);
     }
 }
--- src/main/java/com/cowtowncoder/microb/jackson/JsonNodeReading.java
+++ src/main/java/com/cowtowncoder/microb/jackson/JsonNodeReading.java
@@ -8,6 +8,11 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -21,8 +26,8 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 @BenchmarkMode(Mode.Throughput)
 @State(Scope.Benchmark)
 //During dev, use lower; for real measurements, higher
-@Fork(value = 1)
-//@Fork(value = 3)
+//@Fork(value = 1)
+@Fork(value = 3)
 @Measurement(iterations = 3, time = 3)
 @Warmup(iterations = 3, time = 1)
 public class JsonNodeReading
@@ -35,7 +40,18 @@ public class JsonNodeReading
 
     private final byte[] INPUT_JSON = InputJson.FRIENDS_WITH_VECTORS.bytes();
 
-    private final ObjectMapper JSON_MAPPER = JsonMapper.builder().build();
+    private final ObjectMapper JSON_MAPPER;
+    {
+        JsonFactory f = JsonFactory.builder()
+                .enable(StreamReadFeature.STRICT_DUPLICATE_DETECTION)
+                .enable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
+                .enable(StreamReadFeature.USE_FAST_DOUBLE_PARSER)
+                .enable(StreamWriteFeature.USE_FAST_DOUBLE_WRITER)
+                .build();
+        JSON_MAPPER = JsonMapper.builder(f)
+                .enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)
+                .build();
+    }
 
     /*
     /**********************************************************************
@@ -51,7 +67,9 @@ public class JsonNodeReading
 
     @Benchmark
     public void perfOptimizedReader(Blackhole bh) throws Exception {
-        JsonNode doc = new CustomJsonNodeReader(JSON_MAPPER).readTree(inputJson());
+        JsonParser p = JSON_MAPPER.createParser(inputJson());
+        JsonNode doc = new CustomJsonNodeReader(JSON_MAPPER, p).readTree();
+        p.close();
         bh.consume(doc);
     }
 
@@ -64,7 +82,7 @@ public class JsonNodeReading
     private InputStream inputJson() {
         return new ByteArrayInputStream(INPUT_JSON);
     }
-    
+
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
                 .include(JsonNodeReading.class.getSimpleName())
